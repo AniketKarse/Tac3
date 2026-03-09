08d55eab7407e55aeeb5f81277e940530ba24a90
AniketKarse <aniketkarse712@gmail.com>
2026-03-09 14:30:11 +0000

    feat : making it truely multiplayer

--- composables/useSocket.ts
+++ composables/useSocket.ts
@@ -1,14 +1,15 @@
 import { io, Socket } from 'socket.io-client'
 import { ref } from 'vue'
 
-let socket: Socket | null = null // ek to Socket rahega nahi to null (Crazy!)
+let socket: Socket | null = null 
 
 export function useSocket() {
   const connected = ref(false)
 
-  function connect(url = 'http://localhost:3000') {
+  function connect() {
     if (socket) return socket
-    socket = io(url)
+    
+    socket = io()
 
     socket.on('connect', () => {
       connected.value = true
@@ -16,9 +17,9 @@ export function useSocket() {
 
     return socket
   }
-
+  
   function join(name: string) {
-    socket?.emit('join', { name })
+    socket?.emit('join', name) 
   }
 
   function move(payload: { roomId: string, index: number }) {
